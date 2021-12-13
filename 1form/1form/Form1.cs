2f80f7527b5dc47c9051d7d548cc49c0a9d0e6dc
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-13 15:33:22 +0300

    Add files via upload

---
+++ 1form/1form/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace _1form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            string rex = "[a-zA-Z]+";
+            string str2 = Regex.Replace(str, rex, "...");
+            result.Text=($"Результат:\n {str2}");
+        }
+    }
+}
